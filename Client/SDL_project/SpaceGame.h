0c9fd13d8062377efa5b296a80ec82ea79b24d79
Alastair Rayner <alli0@me.com>
2017-04-27 12:10:49 +0100

    Added user input class

--- Client/SDL_project/SpaceGame.h
+++ Client/SDL_project/SpaceGame.h
@@ -89,7 +89,7 @@ public:
 
 	bool spawnPlayer = true;
 	
-	bool useNetworking = false;
+	bool useNetworking = true;
 	
 
 	//! Network update tick
---
+++ Client/SDL_project/UserInput.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "UserInput.h"
+
+
+UserInput::UserInput()
+{
+}
+
+
+UserInput::~UserInput()
+{
+}
